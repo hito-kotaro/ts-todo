75ed129fe19c2d2f1887ba39a419bc896845e84b
hito-kotaro <tohi.work@gmail.com>
2022-01-11 00:06:16 +0900

    データフェッチの実装まで

--- src/components/TodoApp/TodoList/TodoList.tsx
+++ src/components/TodoApp/TodoList/TodoList.tsx
@@ -1,18 +1,29 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { v4 as uuid } from 'uuid';
 import { useRecoilState } from 'recoil';
 import { todoListState } from '../../../store/todoListState';
 import TodoForm from '../TodoForm/TodoForm';
-import type Todo from '../../../types/Todo';
+import useFetchData from '../../../hooks/useFetchData';
+import useTodoList from '../../../hooks/useTodoList';
+
 // TodoFormとTodoCardの表示を担当
 
 const TodoList = () => {
   const [todoList, setTodoList] = useRecoilState(todoListState);
+  const { isLoading, fetchFromBackEnd } = useFetchData();
 
+  useEffect(() => {
+    fetchFromBackEnd();
+  }, []);
   return (
     <div>
       <h1>header</h1>
       <TodoForm />
-      <h1>TodoCard</h1>
+      <div>
+        {todoList.map((todoData) => (
+          <h1 key={uuid()}>{todoData.title}</h1>
+        ))}
+      </div>
     </div>
   );
 };
