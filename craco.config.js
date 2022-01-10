3924aecd2dce489f340fdc9e9c510c536f2e63e1
hito-kotaro <tohi.work@gmail.com>
2022-01-10 15:45:34 +0900

    first commit

---
+++ craco.config.js
@@ -0,0 +1,9 @@
+/* eslint-disable global-require */
+/* eslint-disable import/no-extraneous-dependencies */
+module.exports = {
+  style: {
+    postcssOptions: {
+      plugins: [require('tailwindcss'), require('autoprefixer')],
+    },
+  },
+};
