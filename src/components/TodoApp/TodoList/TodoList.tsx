05915b373987e92f2e57f77cebdf1bf49eeabf1f
hito-kotaro <tohi.work@gmail.com>
2022-01-13 23:25:15 +0900

    読み込み中のスピナーサイズ変更

--- src/components/TodoApp/TodoList/TodoList.tsx
+++ src/components/TodoApp/TodoList/TodoList.tsx
@@ -20,7 +20,7 @@ const TodoList = memo(() => {
       <Header />
       {isLoading ? (
         <div className="flex justify-center">
-          <div className="mt-10 animate-spin h-10 w-10 border-4 border-blue-500 rounded-full border-t-transparent" />
+          <div className="mt-10 animate-spin h-36 w-36 border-4 border-blue-500 rounded-full border-t-transparent" />
         </div>
       ) : (
         <div>
